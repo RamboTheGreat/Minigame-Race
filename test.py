bf31038a80cc0b034aaf07f254e3172ebd9fe0a5
RamboTheGreat <41248152+RamboTheGreat@users.noreply.github.com>
2018-12-09 16:13:16 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import serial, time, syslog, string
 
 def scoredisp(score):
-	port='/dev/ttyACM0'
-	ard=serial.Serial(port,9600)
+	
+	# initializes the serial port 
+	port = '/dev/ttyACM0'
+	ard = serial.Serial(port,9600)
+	
+	# writes the inputted score to the serial port
 	ard.write(str(score).encode('ascii'))
